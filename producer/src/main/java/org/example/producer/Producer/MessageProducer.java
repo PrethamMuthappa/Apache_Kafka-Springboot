3e7d88fe73330dec9e0314806eedbb1e3d8f9e1e
PrethamMuthappa <prethambachira1819@gmail.com>
2024-04-29 03:02:40 +0530

    Message producer

--- producer/src/main/java/org/example/producer/Producer/MessageProducer.java
+++ producer/src/main/java/org/example/producer/Producer/MessageProducer.java
@@ -1,4 +1,34 @@
 package org.example.producer.Producer;
 
+import org.example.producer.Constants.Myconstants;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.CompletableFuture;
+
+@Component
 public class MessageProducer {
+
+    @Autowired
+    private KafkaTemplate<String,String> kafkaTemplate;
+
+    public void sendBasicMessage(String message){
+        CompletableFuture<SendResult<String , String >>future=this.kafkaTemplate.send(Myconstants.BASIC_MESSAGE,message);
+        try {
+           future.whenComplete((result,ex)->{
+               if(ex==null){
+                   System.out.println("Message sent by producer"+result.getProducerRecord());
+               }
+               else {
+                   System.out.println("Producer message failed"+ex.getMessage());
+               }
+           });
+        }
+        catch (Exception e){
+            System.out.println("Exception occured"+e.getMessage());
+        }
+    }
+
 }
